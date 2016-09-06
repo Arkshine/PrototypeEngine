fc3e6496d3c68d95453d4ec5498baa1870df79e8
Sam <duo929@hotmail.com>
2016-09-06 12:01:35 +0200

    Add Engine files to contain global variables

---
+++ src/engine/Engine.cpp
@@ -0,0 +1,3 @@
+#include "Engine.h"
+
+CCommandBuffer g_CommandBuffer;
---
+++ src/engine/Engine.h
@@ -0,0 +1,11 @@
+#ifndef ENGINE_ENGINE_H
+#define ENGINE_ENGINE_H
+
+#include "console/CCommandBuffer.h"
+
+/**
+*	The command buffer.
+*/
+extern CCommandBuffer g_CommandBuffer;
+
+#endif //ENGINE_ENGINE_H

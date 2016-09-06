e109bcae6fc7150de9c83606443cefee58b7d936
Sam <duo929@hotmail.com>
2016-09-06 12:01:07 +0200

    Add Msg logging function

--- src/common/Logging.cpp
+++ src/common/Logging.cpp
@@ -1,3 +1,6 @@
+#include <cstdarg>
+#include <cstdio>
+
 #include <SDL2/SDL.h>
 
 #include "Logging.h"
@@ -15,4 +18,20 @@ void UTIL_ShowMessageBox( const char* const pszMessage, const char* const pszCap
 	}
 
 	SDL_ShowSimpleMessageBox( type, pszCaption, pszMessage, nullptr );
+}
+
+void Msg( const char* const pszFormat, ... )
+{
+	char szBuffer[ 4096 ];
+
+	va_list list;
+
+	va_start( list, pszFormat );
+
+	const int iResult = vsnprintf( szBuffer, sizeof( szBuffer ), pszFormat, list );
+
+	va_end( list );
+
+	//Log to stdout for now. - Solokiller
+	printf( "%s", szBuffer );
 }
--- src/common/Logging.h
+++ src/common/Logging.h
@@ -12,4 +12,6 @@ enum class LogType
 
 void UTIL_ShowMessageBox( const char* const pszMessage, const char* const pszCaption = "Message", const LogType logType = LogType::INFO );
 
+void Msg( const char* const pszFormat, ... );
+
 #endif //COMMON_LOGGING_H

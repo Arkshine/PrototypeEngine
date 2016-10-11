6b4933bb2a75ef94809c257cca4e5be222647917
Sam <duo929@hotmail.com>
2016-10-11 15:58:30 +0200

    Define ASSERT macro

--- src/common/Common.h
+++ src/common/Common.h
@@ -1,6 +1,15 @@
 #ifndef COMMON_COMMON_H
 #define COMMON_COMMON_H
 
+#include <cassert>
+
 #include "tier1/CommandLine.h"
+
+//TODO: for now, map ASSERT to assert. - Solokiller
+#ifndef NDEBUG
+#define ASSERT assert
+#else
+#define ASSERT
+#endif
 
 #endif //COMMON_COMMON_H
